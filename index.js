be1f3d9721f2aa5473fbb53632d7f9ee21c5f47c
zazzo9039 <zazzo9039@gmail.com>
2019-02-21 15:57:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var sensor = require('node-dht-sensor');
+function time(){
+sensor.read(11, 4, function(err, temperature, humidity) {
+    if (!err) {
+	
+        console.log('temp: ' + temperature.toFixed(2) + '\u00B0C, ' +
+            'humidity: ' + humidity.toFixed(2) + '%'
+        );
+    }
+
+});
+}
+setInterval(time,1500);
